ac461b0898fbe3b3f448d633121108941ac8c108
Juan Ramirez <tuxgabo@gmail.com>
2020-05-09 22:31:07 -0500

    framer and progress bar is here

---
+++ src/components/progress.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Frame } from "framer";
+
+export function ProgressBar() {
+  return (
+    <Frame
+      height={44}
+      width="100%"
+      center
+      borderRadius={22}
+      background="#dedede"
+    />
+  );
+}
