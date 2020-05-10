c34a4ade5d49fa7a713f01673eb8fc89646c7571
Juan Ramirez <tuxgabo@gmail.com>
2020-05-10 17:46:54 -0500

    order list added

---
+++ src/components/orders-list.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+import OrderCard from "./card";
+
+/**
+ *
+ * class component for listing a single order
+ */
+export default function OrdersList({ orders, deleteOrders, vote }) {
+  const cards = () => {
+    return orders.map(order => {
+      return (
+        <OrderCard key={order._id} order={order} deleteOrders={deleteOrders} vote={vote}/>
+      );
+    });
+  };
+  return <Card.Group>{cards()}</Card.Group>;
+}
