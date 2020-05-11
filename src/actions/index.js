60e040c2e1c8d1ac950603e3f1f7640dd83e3735
Juan Ramirez <tuxgabo@gmail.com>
2020-05-11 12:06:41 -0500

    no interpolation can be done in a normal string

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const localBaseURL = `http://localhost:3000`;
 const baseURL= process.env.NODE_ENV === "dev" ? localBaseURL : netlifyAppBaseURL;
 
 export const client = axios.create({
-  baseURL: "${baseURL}/.netlify/functions/api",
+  baseURL: `${baseURL}/.netlify/functions/api`,
   headers: {
     "Content-Type": "application/json"
   }
