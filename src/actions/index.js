14eabeafbba6e11e94d701e06879e8b33c610324
Juan Ramirez <tuxgabo@gmail.com>
2020-05-11 12:03:13 -0500

    base URL programatically on environments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 // src/actions/index.js
 import axios from "axios";
 
+const netlifyAppBaseURL = `https://zen-brahmagupta-169e98.netlify.app`;
+const localBaseURL = `http://localhost:3000`;
+const baseURL= process.env.NODE_ENV === "dev" ? localBaseURL : netlifyAppBaseURL;
+
 export const client = axios.create({
-  baseURL: "http://localhost:3000/.netlify/functions/api",
+  baseURL: "${baseURL}/.netlify/functions/api",
   headers: {
     "Content-Type": "application/json"
   }
